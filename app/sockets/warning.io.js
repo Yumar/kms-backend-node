dcfdeb88448822ffa11cda04df554549a3d724d4
Yumarx Polanco <jumarpolanco@gmail.com>
2016-04-07 22:14:41 +0000

    warning.io.js prepared for filtering warnings

--- app/sockets/warning.io.js
+++ app/sockets/warning.io.js
@@ -1,15 +1,31 @@
 var mongoose = require('mongoose');
 var WarningModel = mongoose.model('Warning');
+var WarningController = require('../controllers/warning.controller');
 
 module.exports = function (io) {
     var warningIO = io
             .of('/warning')
             .on('connection', function (socket) {
-                console.log('connected');
-
-                WarningModel.find(function (err, warnings) {
+                var notificationAreas = [];
+                var sendWarning = function (err, warnings) {
                     if (!err) {
-//                        console.log(warnings);
+                        socket.emit('warning:list', warnings);
+                        console.log('warning list send');
+                    } else {
+                        console.log(err);
+                        socket.emit('warning:error', err);
+                    }
+                }
+
+                if (notificationAreas.length > 0) {
+                    WarningController.getWarningsInArea(notificationAreas, sendWarning);
+                } else {
+                    WarningController.getWarnings(sendWarning);
+                }
+
+
+                WarningController.findRecentWarnings(function (err, warnings) {
+                    if (!err) {
                         socket.emit('warning:list', warnings);
                         console.log('warning list send');
                     }
@@ -17,15 +33,25 @@ module.exports = function (io) {
 
                 socket.on('warning:create', function (data) {
                     var newwarning = new WarningModel(data);
-//                    console.log('new warning recieved', data);
                     newwarning.save(function (err) {
                         if (!err) {
                             console.log('emited new warning');
-                            warningIO.emit('warning:new', newwarning);
-                        }else{
+                            WarningController.notifyObservers(newwarning);
+                        } else {
                             console.log(err);
+                            socket.emit('warning:error', err);
                         }
                     });
+                });
+
+                socket.on('warning:customizeAreas', function (data) {
+                    notificationAreas = data;
+                    WarningController.registerObserver(function (newWarning) {
+                        var inArea = WarningController.isWarningInAreas(newWarning, notificationAreas);
+                        if (inArea) {
+                            socket.emit('warning:new', newWarning);
+                        }
+                    })
                 });
             });
 }
